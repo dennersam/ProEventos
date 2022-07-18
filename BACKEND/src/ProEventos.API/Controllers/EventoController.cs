5f9ee0f6f87fef013ceecdcd9465375fbf8b5a57
Denner <dennermpb@hotmail.com>
2022-07-18 08:57:26 -0300

    Primeiro commit backend eventos exemplos

---
+++ BACKEND/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+
+        }
+
+        private IEnumerable<Evento> _evento = new Evento[]{
+            new Evento(){
+                EventoId = 1,
+                Tema = "Angular 11 e .NET 5",
+                Local = "Belo Horizonte",
+                Lote = "Lote 1",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto.img"
+            },
+            new Evento(){
+                EventoId = 2,
+                Tema = "Angular Updates",
+                Local = "Rio de Janeiro",
+                Lote = "Lote 2",
+                QtdPessoas = 350,
+                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+                ImagemURL = "foto2.img"
+            }
+        };
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Post test!!!";
+        }
+    }
+}
