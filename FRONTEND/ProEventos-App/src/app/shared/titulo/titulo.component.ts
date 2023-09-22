44d16497a64816c082a9c4536000b1d528e6e3ef
Denner <dennermpb@hotmail.com>
2023-09-22 13:57:31 -0300

    fix titulo.scss

--- FRONTEND/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ FRONTEND/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-titulo',
   templateUrl: './titulo.component.html',
-  styleUrls: ['./titulo.component.css']
+  styleUrls: ['./titulo.component.scss']
 })
 export class TituloComponent {
 
